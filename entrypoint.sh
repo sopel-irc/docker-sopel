9a3c97dfe52cd969fa75e8573612bd0974e60049
Humorous Baby <44451911+HumorBaby@users.noreply.github.com>
2019-01-22 08:38:51 -0500

    fix: pass arguments to sopel command
    Flags and arguments are passed to `sopel` command now (e.g., `-h`, `-w`)
    when they are the only arguments to the `docker run` command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-# Run `sopel` with the provided arguments
+# Set arguments/flags for sopel command
 if [ "${#}" -eq 0 ] || [ "${1#-}" != "${1}" ]; then
   set -- sopel "${@}"
 fi
 
-# OR, just run sopel
+# Run `sopel`
 if [ "${1}" = "sopel" ]; then
-  exec sopel
+  exec sopel "${@}"
 fi
 
 # Otherwise, try to run what the user specified.
