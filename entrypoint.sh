fd53f62352423e90878ed83044c1eca9ea67a89c
Humorous Baby <44451911+HumorBaby@users.noreply.github.com>
2019-01-21 21:27:35 -0500

    feat: add entrypoint script
    Adds entrypoint to image to provide a simple, consistent interface in line
    with the Docker philosophy: "A beginning user should be able to
    `docker run sopelirc/sopel bash` (or `sh`) without needing to learn about
    `--entrypoint`. It is also nice for advanced users to take advantage of
    `entrypoint`, so that they can `docker run sopelirc/sopel --arg1 --arg2`
     without having to specify the binary to execute."
    
    Also, this commit moves the build-specific ARGs (e.g., `BUILD_DATE`)
    out of the `docker build` command to allow for cache-bust preventing
    hack-y monkey business. See the comment in `hooks/build` for more details.

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+# Run `sopel` with the provided arguments
+if [ "${#}" -eq 0 ] || [ $"{1#-}" != "${1}" ]; then
+  set -- sopel "${@}"
+fi
+
+# OR, just run sopel
+if [ "${1}" = "sopel" ]; then
+  exec sopel
+fi
+
+# Otherwise, try to run what the user specified.
+exec "${@}"
