c42210cd5777af28f631f1193304b6c84aadd58e
Humorous Baby <44451911+HumorBaby@users.noreply.github.com>
2019-01-29 06:23:23 -0500

    refactor: build as root, run as sopel
    Now uses `su-exec` to drop to non-`root` user for running the bot. This leaves the option to run other commands/scripts as `root` when needed.
    
    Fixes 1 of 3 from #9

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
-set -e
+set -ex
+
+export PATH="/home/sopel/.local/bin:${PATH}"
 
 # Set arguments/flags for sopel command
 if [ "${#}" -eq 0 ] || [ "${1#-}" != "${1}" ]; then
   set -- sopel "${@}"
+fi
+
+if [ "${1}" = "sopel" ]; then
+  exec su-exec sopel "${@}"
+fi
+
+if [ -n "${USER}" ]; then
+  set -- su-exec "${USER}" "${@}"
 fi
 
 exec "${@}"
